7742b6131c4922d222a181a3476fa207f8b348a2
LotusChing <986332598@qq.com>
2016-06-14 21:40:43 +0800

    update check nginx status scripts

--- Scripts/zabbix/check_nginx_status.sh
+++ Scripts/zabbix/check_nginx_status.sh
@@ -9,34 +9,34 @@ function ping {
 }
 
 function active-connections {
-   /usr/bin/curl -s http://192.168.0.162/nginx_status | awk '/Active/ {print $3}'
+   /usr/bin/curl -s http://$HOST:$PORT/status | awk '/Active/ {print $3}'
 }
 
 function process-conn {
-   /usr/bin/curl -s http://192.168.0.162/nginx_status | awk ' NR==3 {print $1} '
+   /usr/bin/curl -s http://$HOST:$PORT/status | awk ' NR==3 {print $1} '
 }
 
 function accepts-handled {
-   /usr/bin/curl -s http://192.168.0.162/nginx_status | awk ' NR==3 {print $2} '
+   /usr/bin/curl -s http://$HOST:$PORT/status | awk ' NR==3 {print $2} '
 }
 
 function process-request {
-   /usr/bin/curl -s http://192.168.0.162/nginx_status | awk ' NR==3 {print $3} '
+   /usr/bin/curl -s http://$HOST:$PORT/status | awk ' NR==3 {print $3} '
 }
 
 function process-request-time {
-   /usr/bin/curl -s http://192.168.0.162/nginx_status | awk ' NR==3 {print $4} '
+   /usr/bin/curl -s http://$HOST:$PORT/status | awk ' NR==3 {print $4} '
 }
 
 function reading-client-header {
-   /usr/bin/curl -s http://192.168.0.162/nginx_status | awk '/Reading/ {print $2}'
+   /usr/bin/curl -s http://$HOST:$PORT/status | awk '/Reading/ {print $2}'
 }
 
 function writing-client-header {
-   /usr/bin/curl -s http://192.168.0.162/nginx_status | awk '/Writing/ {print $4}'
+   /usr/bin/curl -s http://$HOST:$PORT/status | awk '/Writing/ {print $4}'
 }
 
 function waiting-request {
-   /usr/bin/curl -s http://192.168.0.162/nginx_status | awk '/Waiting/ {print $6}'
+   /usr/bin/curl -s http://$HOST:$PORT/status | awk '/Waiting/ {print $6}'
 }
 $1

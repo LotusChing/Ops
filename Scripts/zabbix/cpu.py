5166c50a78a55320d28675ab84c3cfae63088571
LotusChing <986332598@qq.com>
2016-12-14 14:18:07 +0800

    Update cpu.py

--- Scripts/zabbix/cpu.py
+++ Scripts/zabbix/cpu.py
@@ -2,17 +2,21 @@
 import sys
 import time
 import psutil
+import subprocess
 
 def monitor(interval=2):
     cpu_utilization = psutil.cpu_percent()
+
+    ### Get 1,5,15 server load
+    server_load = subprocess.Popen("uptime | awk -F ':' '{print $NF}'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].strip().strip('\n').split(', ')
 
     ### Get CPU Utilization info
     f = open('/proc/stat')
     cpu_t1 = f.readline().strip('\n').strip('cpu+').split()
     time.sleep(interval)
     f = open('/proc/stat')
-    cpu_t2 = f.readline().strip('\n').strip('cpu+').split()
-   
+    cpu_t2 = f.readline().strip('\n').strip('cpu+').split()   
+
     ### Get CPU Use percent
     cpu_total_t1 = float(eval('+'.join(cpu_t1))) 
     cpu_total_t2 = float(eval('+'.join(cpu_t2)))
@@ -35,7 +39,10 @@ def monitor(interval=2):
         'system_space_percent' : round(cpu_item_usage[2]),
         'idle_percent': round(cpu_item_usage[3]),
         'iowait_percent': round(cpu_item_usage[4]),
-        'cpu_percent' : round(cpu_percent)
+        'cpu_percent' : round(cpu_percent),
+        '1m_load': server_load[0],
+        '5m_load': server_load[1],
+        '15m_load': server_load[2]
     }
     return cpu_info
 
